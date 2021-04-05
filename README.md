baa0010b71b41e77c85e36b6894a9b5c10744a13
BasemaRehman <basema.rehman@hotmail.co.uk>
2021-04-05 19:36:29 +0100

    Dealing with Link types

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # recipe-search-engine
 
 Currently implementing a web crawler to find and store relevant websites
+Deals with multiple types of links
 
 To run: php index.php
 
--- index.php
+++ index.php
@@ -8,7 +8,26 @@ function follow($url) {
     $links = $doc->getElementsByTagName("a");
 
     foreach ($links as $link){
-        echo $link->getAttribute("href")."\n";
+        $l = $link->getAttribute("href");
+
+        if (substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//") {
+            $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
+        } else if (substr($l, 0, 2) == "//") {
+            $l = parse_url($url)["scheme"].":".$l;
+        } else if (substr($l, 0, 2) == "./") {
+             $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l, 1);
+        } else if (substr($l, 0, 1) == "#") {
+             $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;
+        }else if (substr($l, 0, 3) == "../") {
+             $l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+        }else if (substr($l, 0, 11) == "javascript:") {
+             continue;
+        }else if (substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http")  {
+             $l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+                 }
+
+
+        echo $l."\n";
     }
 
 }
