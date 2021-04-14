5bb9f7f04fad92af567da37999dba91acb1ce3fd
BasemaRehman <basema.rehman@hotmail.co.uk>
2021-04-14 17:29:40 +0100

    Starting to insert sites

--- index.php
+++ index.php
@@ -1,84 +1,88 @@
 <?php
-$start = "https://google.com";
+$start = "http://localhost:8888/Bakery_Search_Engine/sites.html";
+$pdo = new PDO('mysql:host=127.0.0.1;port=8889;dbname=RecipeSearch;', 'root', 'root');
 
-$already_crawled = array();
+$crawled_site = array();
 $crawling = array();
 
+function add_test(){
+    global $pdo;
+    $dat = ['id' => '2', 'text' => 'newtext',];
+    $sql = "INSERT INTO test (id, text) VALUES (:id,:text)";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute($dat);
+}
 
-function get_details ($url){
-     $options = array('http' =>array('method'=>"GET", 'headers'=>"User-Agent: BakeryBot/0.1\n"));
-     $context = stream_context_create($options);
+function get_details($url) {
 
-     $doc = new DOMDocument();
-     @$doc->loadHTML(@file_get_contents($url, false, $context));
+	$options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: howBot/0.1\n"));
+	$context = stream_context_create($options);
+	$doc = new DOMDocument();
+	@$doc->loadHTML(@file_get_contents($url, false, $context));
 
-     $title = $doc->getElementsByTagName("title");
-     $title = $title->item(0)->nodeValue;
+	$title = $doc->getElementsByTagName("title");
+	$title = $title->item(0)->nodeValue;
+	$description = "";
+	$keywords = "";
+	$metas = $doc->getElementsByTagName("meta");
+	for ($i = 0; $i < $metas->length; $i++) {
+		$meta = $metas->item($i);
+		if (strtolower($meta->getAttribute("name")) == "description")
+			$description = $meta->getAttribute("content");
+		if (strtolower($meta->getAttribute("name")) == "keywords")
+			$keywords = $meta->getAttribute("content");
 
-     $description = "";
-     $keywords = "";
-     $metas = $doc->getElementsByTagName("meta");
-     for($i = 0; $i<$metas->length; $i++){
-        $meta = $metas->item($i);
-
-        if (strtolower($meta->getAttribute("name")) == "description")
-            $description = $meta->getAttribute("content");
-        if (strtolower($meta->getAttribute("name")) == "keywords")
-            $keywords = $meta->getAttribute("content");
-
-      }
-
-
-	return '{ "Title": "'.str_replace("\n", "", $title).'", "Description": "'.str_replace("\n", "", $description).'", "Keywords": "'.str_replace("\n", "", $keywords).'", "URL": "'.$url.'"},';
+	}
+	return '{ "Title": "'.str_replace("\n", "", $title).'", "Description": "'.str_replace("\n", "", $description).'", "Keywords": "'.str_replace("\n", "", $keywords).'", "URL": "'.$url.'"}';
 
 }
 
 function follow($url) {
+	global $crawled_site;
+	global $crawling;
+	global $pdo;
+	$options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: howBot/0.1\n"));
+	$context = stream_context_create($options);
+	$doc = new DOMDocument();
+	@$doc->loadHTML(@file_get_contents($url, false, $context));
+	$linklist = $doc->getElementsByTagName("a");
+	foreach ($linklist as $link) {
+		$l =  $link->getAttribute("href");
+		if (substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
+		} else if (substr($l, 0, 2) == "//") {
+			$l = parse_url($url)["scheme"].":".$l;
+		} else if (substr($l, 0, 2) == "./") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l, 1);
+		} else if (substr($l, 0, 1) == "#") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;
+		} else if (substr($l, 0, 3) == "../") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+		} else if (substr($l, 0, 11) == "javascript:") {
+			continue;
+		} else if (substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http") {
+			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+		}
+		if (!in_array($l, $crawled_site)) {
+				$crawled_site[] = $l;
+				$crawling[] = $l;
+				$details = json_decode(get_details($l));
 
-    global $already_crawled;
-    global $crawling;
 
-    $options = array('http' =>array('method'=>"GET", 'headers'=>"User-Agent: BakeryBot/0.1\n"));
-    $context = stream_context_create($options);
+				$details->URL;
+				$rows = $pdo->query("SELECT * FROM `recipe_index` WHERE url= '$details->URL'");
+				$rows = $rows->fetchColumn();
 
-    $doc = new DOMDocument();
-    @$doc->loadHTML(@file_get_contents($url, false, $context));
 
-    $links = $doc->getElementsByTagName("a");
 
-    foreach ($links as $link){
-        $l = $link->getAttribute("href");
+		}
 
-        if (substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//") {
-            $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
-        } else if (substr($l, 0, 2) == "//") {
-            $l = parse_url($url)["scheme"].":".$l;
-        } else if (substr($l, 0, 2) == "./") {
-             $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l, 1);
-        } else if (substr($l, 0, 1) == "#") {
-             $l = parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;
-        }else if (substr($l, 0, 3) == "../") {
-             $l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
-        }else if (substr($l, 0, 11) == "javascript:") {
-             continue;
-        }else if (substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http")  {
-             $l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
-                 }
+	}
 
-        if (!in_array($l, $already_crawled)) {
-            $already_crawled[] = $l;
-            $crawling[] = $l;
-            echo get_details($l)."\n";
-            //echo $l."\n";
-        }
-
-    }
-    array_shift($crawling);
-    foreach ($crawling as $site) {
-        follow($site);
-    }
+	array_shift($crawling);
+	foreach ($crawling as $site) {
+		follow($site);
+	}
 
 }
-
-follow($start);
-print_r($already_crawled);
+add_test();
