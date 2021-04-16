ec5abe6550a79d1c8cc767bca176a9e35f9c02a1
BasemaRehman <basema.rehman@hotmail.co.uk>
2021-04-16 17:11:42 +0100

    search page with working links

--- index.php
+++ index.php
@@ -1,91 +1,18 @@
-<?php
-$start = "http://localhost:8888/Bakery_Search_Engine/sites.html";
-$pdo = new PDO('mysql:host=127.0.0.1;port=8889;dbname=RecipeSearch;', 'root', 'root');
-
-$crawled_site = array();
-$crawling = array();
-
-function get_details($url) {
-
-	$options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: howBot/0.1\n"));
-	$context = stream_context_create($options);
-	$doc = new DOMDocument();
-	@$doc->loadHTML(@file_get_contents($url, false, $context));
-
-	$title = $doc->getElementsByTagName("title");
-	$title = $title->item(0)->nodeValue;
-	$description = "";
-	$keywords = "";
-	$metas = $doc->getElementsByTagName("meta");
-	for ($i = 0; $i < $metas->length; $i++) {
-		$meta = $metas->item($i);
-		if (strtolower($meta->getAttribute("name")) == "description")
-			$description = $meta->getAttribute("content");
-		if (strtolower($meta->getAttribute("name")) == "keywords")
-			$keywords = $meta->getAttribute("content");
-
-	}
-	return '{ "Title": "'.str_replace("\n", "", $title).'", "Description": "'.str_replace("\n", "", $description).'", "Keywords": "'.str_replace("\n", "", $keywords).'", "URL": "'.$url.'"}';
-
-}
-
-function follow($url) {
-	global $crawled_site;
-	global $crawling;
-	global $pdo;
-	$options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: howBot/0.1\n"));
-	$context = stream_context_create($options);
-	$doc = new DOMDocument();
-	@$doc->loadHTML(@file_get_contents($url, false, $context));
-	$linklist = $doc->getElementsByTagName("a");
-	foreach ($linklist as $link) {
-		$l =  $link->getAttribute("href");
-		if (substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//") {
-			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
-		} else if (substr($l, 0, 2) == "//") {
-			$l = parse_url($url)["scheme"].":".$l;
-		} else if (substr($l, 0, 2) == "./") {
-			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l, 1);
-		} else if (substr($l, 0, 1) == "#") {
-			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;
-		} else if (substr($l, 0, 3) == "../") {
-			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
-		} else if (substr($l, 0, 11) == "javascript:") {
-			continue;
-		} else if (substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http") {
-			$l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
-		}
-		if (!in_array($l, $crawled_site)) {
-				$crawled_site[] = $l;
-				$crawling[] = $l;
-				$details = json_decode(get_details($l));
-				echo $details->Keywords;
-
-
-				$details->URL;
-				$rows = $pdo->query("SELECT * FROM `recipe_index` WHERE url= '$details->URL'");
-				$rows = $rows->fetchColumn();
-
-				$data = ['title' => $details->Title, 'description' => $details->Description, 'keywords' => $details->Keywords, 'url' => $details->URL, 'url_hash'=> md5($details->URL),];
-
-				if ($rows > 0){
-				    echo "UPDATE";
-
-				} else {
-				// This adds in a new row to the database. Even though there is an id column, it is autoincremented so
-				// need to set a value in the code
-                    $sql = "INSERT INTO recipe_index (title, description, keywords, url, url_hash) VALUES (:title,:description,:keywords,:url,:url_hash)";
-                    $stmt= $pdo->prepare($sql);
-                    $stmt->execute($data);
-				}
-		}
-
-	}
-
-	array_shift($crawling);
-	foreach ($crawling as $site) {
-		follow($site);
-	}
-
-}
-follow($start);
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+             RecipeSearch
+        </title>
+    </head>
+    <body>
+        <div style="width: 600px; margin: 10px auto">
+            <h1 style="text-align: centre;">Search for recipes with RecipeSearch!</h1>
+            <form action="search.php" method="get">
+                <input type= "text" style="width: 100%; font-size: 16px;" name="q" value="" placeholder="Search for recipes...">
+                <div style= "text-align: centre"><input type="submit" style="font-size: 16px; margin: 0px auto; width:100px" name="search" value="Search">
+            </form>
+        </div>
+    </body>
+</html>
---
+++ search.php
@@ -0,0 +1,42 @@
+<?php
+
+$pdo = new PDO('mysql:host=127.0.0.1;port=8889;dbname=RecipeSearch;', 'root', 'root');
+
+$search = $_GET['q'];
+
+$searche = explode(" ", $search);
+
+$x = 0;
+$construct = "";
+$params = array();
+foreach ($searche as $term){
+    $x++;
+    if ($x == 1) {
+        $construct .= "title LIKE CONCAT('%',:search$x,'%') OR description LIKE CONCAT('%',:search$x,'%') or keywords LIKE CONCAT('%',:search$x,'%')";
+    } else {
+        $construct .= " AND title LIKE CONCAT('%',:search$x,'%') OR description LIKE CONCAT('%',:search$x,'%') or keywords LIKE CONCAT('%',:search$x,'%')";
+    }
+    $params["search$x"] = $term;
+}
+
+$results = $pdo->prepare("SELECT * FROM `recipe_index` WHERE $construct");
+$results->execute($params);
+
+if ($results->rowCount() == 0){
+    echo "0 results found";
+} else {
+    echo $results->rowCount()." results found! <hr />";
+}
+foreach ($results->fetchAll() as $result){
+    $title = $result ['title'];
+    if ($result ['description'] == ""){
+        $desc = "No description available";
+    } else {
+        $desc = $result ['description'];
+    }
+
+    $url = $result ['url'];
+    echo "<a href='$url'> $title </a> <br> $desc <br> <a href='$url'> $url </a><hr />";
+
+
+}
